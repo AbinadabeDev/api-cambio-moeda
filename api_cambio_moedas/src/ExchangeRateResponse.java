c76e087fe0d7ccca71225066f479b25185b3aeca
Abinadabe <abinadabedev@outlook.com>
2025-01-10 12:29:23 -0300

    commit base

---
+++ api_cambio_moedas/src/ExchangeRateResponse.java
@@ -0,0 +1,5 @@
+class ExchangeRateResponse {
+    String result;
+    String base_code;
+    // Adicione os campos necessários com base na estrutura do JSON retornado
+}
--- api_cambio_moedas/src/Principal.java
+++ api_cambio_moedas/src/Principal.java
@@ -1,12 +1,7 @@
-import com.google.gson.Gson;
-
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -16,23 +11,19 @@ public class Principal {
         System.out.println("Digite ou cole sua chave key: ");
         var key = leitura.nextLine();
 
-       URI endereco = URI.create("https://v6.exchangerate-api.com/v6/" + key + "/latest/USD");
+        URI endereco = URI.create("https://v6.exchangerate-api.com/v6/" + key + "/latest/USD");
         leitura.close();
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(endereco)
                 .build();
 
-        try {
-            HttpResponse<String> response = HttpClient
-                    .newHttpClient()
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-            //return new Gson().fromJson(response.body(), Moeda.class);
-
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            // Exibe a resposta do servidor
             System.out.println(response.body());
         } catch (Exception e) {
-            throw new RuntimeException("Não consegui obter o endereço a partir da key: " + key);
+            throw new RuntimeException("Não consegui obter o endereço a partir da key: " + key, e);
         }
-
     }
 }
